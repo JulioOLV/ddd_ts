31491f6c4051d0d2936c121c6e1dff78d3585979
JulioOLV <julioccolv@gmail.com>
2024-04-02 15:57:26 -0300

    feat: add find product use case

---
+++ src/usecase/product/find/find.product.usecase.ts
@@ -0,0 +1,20 @@
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import { InputFindProductDto, OutputFindProductDto } from "./find.product.dto";
+
+export default class FindProductUseCase {
+  private productRepository: ProductRepositoryInterface;
+
+  constructor(productRepository: ProductRepositoryInterface) {
+    this.productRepository = productRepository;
+  }
+
+  async execute(input: InputFindProductDto): Promise<OutputFindProductDto> {
+    const result = await this.productRepository.find(input.id);
+
+    return {
+      id: result.id,
+      name: result.name,
+      price: result.price,
+    };
+  }
+}
