0787be9710c343d1874a41cd48984e603112dbb5
JulioOLV <julioccolv@gmail.com>
2024-04-02 16:41:25 -0300

    feat: add new list product use case

---
+++ src/usecase/product/list/list.product.usecase.ts
@@ -0,0 +1,29 @@
+import Product from "../../../domain/product/entity/product";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import { InputListProductDto, OutputListProductDto } from "./list.product.dto";
+
+export default class ListProductUseCase {
+  private productRepository: ProductRepositoryInterface;
+
+  constructor(productRepository: ProductRepositoryInterface) {
+    this.productRepository = productRepository;
+  }
+
+  async execute(input: InputListProductDto): Promise<OutputListProductDto> {
+    const productList = await this.productRepository.findAll();
+
+    return OutputMapper.toOutput(productList);
+  }
+}
+
+class OutputMapper {
+  static toOutput(productList: Product[]): OutputListProductDto {
+    return {
+      products: productList.map((product: Product) => ({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+      }))
+    };
+  }
+}
